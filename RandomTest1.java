72bd55b855e89fa9f3ffd780eb65f269ca860568
Ndacyayisenga-droid <ndacyayinoah@gmail.com>
2025-08-27 05:25:22 +0300

    run workflow

---
+++ RandomTest1.java
@@ -0,0 +1,7 @@
+package test;
+
+public class RandomTest1 {
+    public static void main(String[] args) {
+        System.out.println("Hello from RandomTest1!");
+    }
+}
---
+++ RandomTest2.java
@@ -0,0 +1,7 @@
+package test;
+
+public class RandomTest2 {
+    public static void main(String[] args) {
+        System.out.println("Hello from RandomTest2!");
+    }
+}
